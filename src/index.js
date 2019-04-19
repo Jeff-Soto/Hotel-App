36ec1eae9771ca6a17a3c428003fab7931cbacd3
Aly Soto <aly@Alys-Air.home>
2019-04-19 15:27:24 -0400

    add sass and bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './components/App';
+const bootstrap = require('bootstrap');
+require('bootstrap/dist/css/bootstrap.css');
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
