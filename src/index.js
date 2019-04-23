60835935813e69d4bfac32e38db2e4da056db26c
Aly Soto <aly@Alys-Air.home>
2019-04-23 08:37:03 -0400

    create redux store

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/App';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { reducer } from './reducers'
 const bootstrap = require('bootstrap');
 require('bootstrap/dist/css/bootstrap.css');
 
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
